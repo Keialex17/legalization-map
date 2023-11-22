ed6996064488600331d71898b7cd9cd27becdb04
Keiber Hernández <115053607+Keialex17@users.noreply.github.com>
2023-11-22 22:28:28 +0000

    ntroduccion del mapa, creacion del repo

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,13 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Map from "../component/map";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Mapa de legalizacion mundial del Cannabis!!</h1>
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
@@ -16,10 +17,7 @@ export const Home = () => {
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
 			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
+				<Map/>
 			</p>
 		</div>
 	);
