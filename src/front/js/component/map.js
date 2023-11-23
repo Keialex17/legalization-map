a0fd1dc96aa2f2d65791f0efab4a24316290ddc7
Keiber Hernández <115053607+Keialex17@users.noreply.github.com>
2023-11-23 00:05:14 +0000

    intentando con geoJSON

--- src/front/js/component/map.js
+++ src/front/js/component/map.js
@@ -1,32 +1,79 @@
 import React from "react";
 import WorldMap from "react-svg-worldmap"
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+} from "react-simple-maps";
 
 const Map = () =>{
 
     const data = [
-        { country: "cn", value: 1389618778 }, // china
-        { country: "in", value: 1311559204 }, // india
-        { country: "us", value: 331883986 }, // united states
-        { country: "id", value: 264935824 }, // indonesia
-        { country: "pk", value: 210797836 }, // pakistan
-        { country: "br", value: 210301591 }, // brazil
-        { country: "ng", value: 208679114 }, // nigeria
-        { country: "bd", value: 161062905 }, // bangladesh
-        { country: "ru", value: 141944641 }, // russia
-        { country: "mx", value: 127318112 }, // mexico
+        { country: "cn", value: "si" }, // china
+        { country: "in", value: "no" }, // india
+        { country: "us", value: "si" }, // united states
+        { country: "id", value: "no se" }, // indonesia
+        { country: "pk", value: "si" }, // pakistan
+        { country: "br", value: "no" }, // brazil
+        { country: "ng", value: "no" }, // nigeria
+        { country: "bd", value: "si" }, // bangladesh
+        { country: "ru", value: "no" }, // russia
+        { country: "mx", value: "no se" }, // mexico
+        { country: "ve", value: "no" }, // venezuela
       ];
+
+      const getColor = (value) => {
+        if (value === "si") {
+          return "green";
+        } else if (value === "no") {
+          return "red";
+        } else {
+          return "gray";
+        }
+      };
 
     return (
         <>
-        <div className="container"> 
-        <WorldMap
+        <div className="container">
+
+      <ComposableMap
+        projectionConfig={{
+          scale: 200,
+        }}
+      >
+        <Geographies geography={"https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              const { ISO_A2 } = geo.properties;
+              console.log(ISO_A2)
+              const countryData = data.find((d) => d.country === ISO_A2);
+              console.log(countryData)
+              const color = countryData ? getColor(countryData.value) : "blue";
+              console.log(color)
+
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={color}
+                  stroke="white"
+                  strokeWidth={0.5}
+                />
+              );
+            })
+          }
+        </Geographies>
+      </ComposableMap>
+
+        </div>
+        {/* <WorldMap
         color="green"
         title="Top 10 Populous Countries"
-        value-suffix="people"
+        value-suffix="legal"
         size="xxl"
         data={data}
-      />
-        </div>
+      /> */}
         </>
     )
 }
