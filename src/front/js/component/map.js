96d6e6574979812267d3f7acbab8318fbcf332df
Keiber Hernández <115053607+Keialex17@users.noreply.github.com>
2023-11-26 21:47:54 +0000

    nuevas ramas creadas

--- src/front/js/component/map.js
+++ src/front/js/component/map.js
@@ -1,11 +1,5 @@
 import React from "react";
 import WorldMap from "react-svg-worldmap"
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Marker,
-} from "react-simple-maps";
 
 const Map = () =>{
 
@@ -21,59 +15,21 @@ const Map = () =>{
         { country: "ru", value: "no" }, // russia
         { country: "mx", value: "no se" }, // mexico
         { country: "ve", value: "no" }, // venezuela
+        { country: "ar", value: "si" }, // venezuela
       ];
-
-      const getColor = (value) => {
-        if (value === "si") {
-          return "green";
-        } else if (value === "no") {
-          return "red";
-        } else {
-          return "gray";
-        }
-      };
 
     return (
         <>
         <div className="container">
 
-      <ComposableMap
-        projectionConfig={{
-          scale: 200,
-        }}
-      >
-        <Geographies geography={"https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const { ISO_A2 } = geo.properties;
-              console.log(ISO_A2)
-              const countryData = data.find((d) => d.country === ISO_A2);
-              console.log(countryData)
-              const color = countryData ? getColor(countryData.value) : "blue";
-              console.log(color)
-
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={color}
-                  stroke="white"
-                  strokeWidth={0.5}
-                />
-              );
-            })
-          }
-        </Geographies>
-      </ComposableMap>
-
-        </div>
-        {/* <WorldMap
-        color="green"
-        title="Top 10 Populous Countries"
-        value-suffix="legal"
-        size="xxl"
-        data={data}
-      /> */}
+        <WorldMap
+   color="green"
+   title="Top 10 Populous Countries"
+   value-suffix="legal"
+   size="xxl"
+   data={data}
+ /> 
+         </div>
         </>
     )
 }
