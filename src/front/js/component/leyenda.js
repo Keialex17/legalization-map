29586b57dcc6ffb3cae61afc458953c2fdc1072a
Keiiber24 <101483657+Keiiber24@users.noreply.github.com>
2023-12-11 06:22:09 +0000

    Home arreglado, leyenda arreglada y asia y oceania agg, tambien coloque ilegal los paises faltantes

--- src/front/js/component/leyenda.js
+++ src/front/js/component/leyenda.js
@@ -9,21 +9,30 @@ const Leyenda = () =>{
       };
 
     return(
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
-          <div style={{ width: '20px', height: '20px', backgroundColor: legendColors.si, marginRight: '5px' }}></div>
-          <span>Sí</span>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
-          <div style={{ width: '20px', height: '20px', backgroundColor: legendColors.resi, marginRight: '5px' }}></div>
-          <span>Residencial</span>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ width: '20px', height: '20px', backgroundColor: legendColors.no, marginRight: '5px' }}></div>
-          <span>No</span>
-        </div>
-        <h2>Date of data: 06/2022</h2>
+      //   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+      //   <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+      //     <div style={{ width: '20px', height: '20px', backgroundColor: legendColors.si, marginRight: '5px' }}></div>
+      //     <span>Sí</span>
+      //   </div>
+      //   <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+      //     <div style={{ width: '20px', height: '20px', backgroundColor: legendColors.resi, marginRight: '5px' }}></div>
+      //     <span>Residencial</span>
+      //   </div>
+      //   <div style={{ display: 'flex', alignItems: 'center' }}>
+      //     <div style={{ width: '20px', height: '20px', backgroundColor: legendColors.no, marginRight: '5px' }}></div>
+      //     <span>No</span>
+      //   </div>
+      //   <h2>Date of data: 06/2022</h2>
+      // </div>
+
+      
+      <div className="col-3 d-flex justify-content-around">
+        <span className="btn btn-success">Legal</span>
+        <span className="btn btn-warning">Residencial</span>
+        <span className="btn btn-danger">Ilegal</span>
       </div>
+
+
     )
 }
 
